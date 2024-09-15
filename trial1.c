3777990120864c024df0f9c7fd799c00af1f474a
SABHYAGUPTA5XR <sabhyagupta4xr@gmail.com>
2024-09-15 17:57:48 +0530

    Added another feature called feature 4

--- trial1.c
+++ trial1.c
@@ -1,9 +1,6 @@
 #include<stdio.h>
 int main()
 {
-    printf("I am Feature1!!\n");
-    printf("I am actually at feature2!!\n");
-    printf("This is what i actually wanted!\n");
-    printf("This is my main now\n");    
+    printf("I am feature 4");
     return 0;
 }
