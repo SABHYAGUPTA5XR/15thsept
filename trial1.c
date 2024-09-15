b159fcd0970432324ff9911d20d4be3975af2f47
SABHYAGUPTA5XR <sabhyagupta4xr@gmail.com>
2024-09-15 17:32:04 +0530

    Final answer

--- trial1.c
+++ trial1.c
@@ -3,5 +3,7 @@ int main()
 {
     printf("I am Feature1!!\n");
     printf("I am actually at feature2!!\n");
+    printf("This is what i actually wanted!\n");
+    printf("This is my main now\n");    
     return 0;
 }
